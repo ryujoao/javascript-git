bac81f0f3d47df52e95aa97b5b3bda7a77a49feb
ryujoao <joaoryu2008@gmail.com>
2024-06-27 11:46:05 -0300

    sistema do aluno

---
+++ 27-06-24/sistema-aluno/script.js
@@ -0,0 +1,19 @@
+const button = document.querySelector("button");
+const inputNota = document.querySelector("#nota");
+ 
+
+// Criando uma função
+button.addEventListener("click", function(event) {
+    event.preventDefault();
+    console.log(inputNota.value);
+
+if (inputNota.value >= 6) {
+    alert("Você está aprovado");
+} else {
+    alert("Você está reprovado");
+
+}
+   
+
+});
+
