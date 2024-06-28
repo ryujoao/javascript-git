9ee5b502531495d3a027beb25c3cae3d43c981a2
ryujoao <joaoryu2008@gmail.com>
2024-06-28 11:45:33 -0300

    loops, incremento e decremento

---
+++ 28-06-24/decremento.js
@@ -0,0 +1,11 @@
+let valor = 7;
+console.log(valor);// valendo 7
+valor--;
+console.log(valor);// valendo 6
+valor -= 4;
+console.log(valor);// valendo 2
+// lógica
+valor *= 4;
+console.log(valor);
+valor /= 8;
+console.log(valor);
---
+++ 28-06-24/exerciciofor-01.js
@@ -0,0 +1,12 @@
+/*
+- Escreva um loop (for) que exiba a frase abaixo no console;
+- Substitua o "X" pela informação correta;
+- "X" deve começar em 1 na 1º exibição da frase e em 10 na última;
+- Exiba a frase 10x no console.
+
+"Esta é a Xº vez que esta frase é exibida no console."
+*/
+
+for (let phrase = 0; phrase <= 10; phrase++) {
+    console.log(`Esta é a ${phrase}º vez que esta frase é exibida no console. `);
+}
