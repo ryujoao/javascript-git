1c7c1ce77e5d111d24b90d85824d47eb1d848ec7
ryujoao <joaoryu2008@gmail.com>
2024-06-28 10:36:07 -0300

    sistema aluno

--- 27-06-24/sistema-aluno/script.js
+++ 27-06-24/sistema-aluno/script.js
@@ -1,19 +1,37 @@
 const button = document.querySelector("button");
 const inputNota = document.querySelector("#nota");
- 
 
+// criando uma função para limpar o campo
+function clearForm() {
+    inputNotaValue = "";
+}
+ 
 // Criando uma função
 button.addEventListener("click", function(event) {
     event.preventDefault();
-    console.log(inputNota.value);
+    const inputNotaValue = inputNota.value;
+    //console.log(inputNotaValue);
 
-if (inputNota.value >= 6) {
+// validação de formulário
+if (inputNotaValue === '') {
+    alert("Campo obrigatório");
+    return false;
+}
+
+
+
+// lógica    
+if (inputNotaValue >= 6) {
     alert("Você está aprovado");
 } else {
     alert("Você está reprovado");
 
 }
-   
 
+// chamando a função limpar campo
+clearForm();
+
+
+   
 });
 
